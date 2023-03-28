e15f9631b11ecbb569defc5bc36af74126cf8c03
eptate01 <ethanptate@gmail.com>
2023-03-28 13:49:59 -0400

    Small update

--- RNN.py
+++ RNN.py
@@ -6,6 +6,78 @@ import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_sequence
 import numpy as np
 
+BATCH_SIZE = 62
+
+
+def one_hot(sequence, dictionary_size):
+    encoding = np.zeros((BATCH_SIZE,len(sequence), dictionary_size), dtype=np.float32)
+    for i in range(len(sequence)):
+        encoding[0 ,i, sequence[i]] = 1
+    return encoding
+
+class RNNModel(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size, num_layers):
+        super(RNNModel, self).__init__()
+        
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        
+        #Define the network!
+		#Batch first defines where the batch parameter is in the tensor
+        #self.embedding = nn.embedding(...)
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first = True)
+        self.fc = nn.Linear(hidden_size, output_size)
+        
+
+    def forward(self, x):
+        hidden_state = self.init_hidden()
+        output,hidden_state = self.rnn(x, hidden_state)
+		#Shouldn't need to resize if using batches, this eliminates the first dimension
+        print(output.size())
+        #output = output.contiguous().view(-1, self.hidden_size)
+        print(output.size())
+        output = self.fc(output)
+        return output, hidden_state
+        
+    def init_hidden(self):
+        #Hey,this is our hidden state. Hopefully if we don't have a batch it won't yell at us
+        #Also a note, pytorch, by default, wants the batch index to be the middle dimension here. 
+        #So it looks like (row, BATCH, column)
+        hidden = torch.zeros(self.num_layers, BATCH_SIZE, self.hidden_size)
+        return hidden
+
+def predict(model, character):
+    
+    characterInput = np.array([charInt[c] for c in character])
+    #print(characterInput)
+    characterInput = one_hot(characterInput, dictionary_size)
+    #print(characterInput)
+    characterInput = torch.from_numpy(characterInput)
+    #print(character)
+    out, hidden = model(characterInput)
+    
+    #Get output probabilities
+    
+    prob = nn.functional.softmax(out[-1], dim=0).data
+    #print(prob)
+    character_index = torch.max(prob, dim=0)[1].item()
+    
+    return intChar[character_index], hidden
+    
+def sample(model, out_len, start='MENENIUS'):
+    characters = [ch for ch in start]
+    currentSize = out_len - len(characters)
+    for i in range(currentSize):
+        character, hidden_state = predict(model, characters)
+        characters.append(character)
+        
+    return ''.join(characters)
+
+
+
+
+
+
 sentences =[]
 
 with open('tiny-shakespeare.txt', "r") as file:
@@ -61,45 +133,11 @@ for i in range(len(sentences)):
     input_sequence[i] = [charInt[character] for character in input_sequence[i]]
     target_sequence[i] = [charInt[character] for character in target_sequence[i]]
 
-def one_hot(sequence, dictionary_size):
-    encoding = np.zeros((1,len(sequence), dictionary_size), dtype=np.float32)
-    for i in range(len(sequence)):
-        encoding[0 ,i, sequence[i]] = 1
-    return encoding
-
-class RNNModel(nn.Module):
-    def __init__(self, input_size, output_size, hidden_size, num_layers):
-        super(RNNModel, self).__init__()
-        
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        
-        #Define the network!
-		#Batch first defines where the batch parameter is in the tensor
-        #self.embedding = nn.embedding(...)
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first = True)
-        self.fc = nn.Linear(hidden_size, output_size)
-        
-
-    def forward(self, x):
-        hidden_state = self.init_hidden()
-        output,hidden_state = self.rnn(x, hidden_state)
-		#Shouldn't need to resize if using batches, this eliminates the first dimension
-        output = output.contiguous().view(-1, self.hidden_size)
-        output = self.fc(output)
-        return output, hidden_state
-        
-    def init_hidden(self):
-        #Hey,this is our hidden state. Hopefully if we don't have a batch it won't yell at us
-        #Also a note, pytorch, by default, wants the batch index to be the middle dimension here. 
-        #So it looks like (row, BATCH, column)
-        hidden = torch.zeros(self.num_layers, 1, self.hidden_size)
-        return hidden
-
 
 # Main Code
 dictionary_size = len(charInt)
-one_hot(input_sequence[0], dictionary_size)
+one_hot(input_sequence[0:BATCH_SIZE-1], dictionary_size)
+
 
 model = RNNModel(dictionary_size, dictionary_size, 100, 1)
 
@@ -122,6 +160,7 @@ for epoch in range(1):
         #print(hidden)
         #print(output.size())
         #print(x.view(-1).long().size())
+        print(y.view(-1).long().size())
         lossValue = loss(output, y.view(-1).long())
 		#Calculates gradient
         lossValue.backward()
@@ -130,31 +169,6 @@ for epoch in range(1):
         
         #print("Loss: {:.4f}".format(lossValue.item()))
 
-def predict(model, character):
-    
-    characterInput = np.array([charInt[c] for c in character])
-    #print(characterInput)
-    characterInput = one_hot(characterInput, dictionary_size)
-    #print(characterInput)
-    characterInput = torch.from_numpy(characterInput)
-    #print(character)
-    out, hidden = model(characterInput)
-    
-    #Get output probabilities
-    
-    prob = nn.functional.softmax(out[-1], dim=0).data
-    #print(prob)
-    character_index = torch.max(prob, dim=0)[1].item()
-    
-    return intChar[character_index], hidden
-    
-def sample(model, out_len, start='MENENIUS'):
-    characters = [ch for ch in start]
-    currentSize = out_len - len(characters)
-    for i in range(currentSize):
-        character, hidden_state = predict(model, characters)
-        characters.append(character)
-        
-    return ''.join(characters)
+
     
 print(sample(model, 50))
